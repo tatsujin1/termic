9c01960a48c5e8788295c1bbf96981feeecabc7e
André Jonsson <andre.jonsson@dataductus.se>
2021-12-28 11:47:32 +0100

    rainbow slightly duller to make text legible
    now also shows latest key pressed

--- examples/demo1/main.cpp
+++ examples/demo1/main.cpp
@@ -29,25 +29,32 @@ int main()
 	Canvas canvas(app.screen());
 	color::LinearGradient gradient({
 		color::Black,
-		color::Yellow,
-		color::Blue,
-		color::Red,
-		color::Cyan,
-		color::Purple,
-		color::White,
-		color::Green,
+		color::rgb(180, 180, 20),
+		color::rgb(20, 20, 180),
+		color::rgb(180, 20, 20),
+		color::rgb(20, 180, 180),
+		color::rgb(180, 20, 180),
+		color::rgb(180, 180, 180),
+		color::rgb(20, 180, 180),
+		color::Black,
 	});
 
 	float rotation { 45 };
 	float offset { 0 };
 
-	auto render_demo = [&app, &canvas, &rotation, &gradient, &offset]() {
+	auto render_demo = [&app, &canvas, &rotation, &gradient, &offset](key::Key key=key::None, key::Modifier mods=key::NoMod) {
 		canvas.clear();
-		app.screen().print({ 10, 10 }, "Yeah!", color::Black, color::NoChange);
+		app.screen().print({ 10, 10 }, "Termic rainbow demo!", color::White, color::NoChange);
 		gradient.set_offset(offset);
 		canvas.fill(&gradient, rotation);
-		app.screen().print({ 6, 12 }, "Things and stuff...", color::Red, color::NoChange);
+		app.screen().print({ 6, 12 }, "Things and stuff...", color::rgb(255, 50, 50), color::NoChange);
 		app.screen().print({ 70, 20 }, "TERMIC", color::Green, color::NoChange);
+
+		if(key != key::None)
+		{
+			const auto key_str = key::to_string(key, mods);
+			app.screen().print({ 25, 15 }, fmt::format("Key pressed: {}", key_str), color::White, color::NoChange);
+		}
 	};
 
 	app.on_app_start.connect([&render_demo]() {
@@ -60,31 +67,22 @@ int main()
 		if(k.key == key::ESCAPE and k.modifiers == key::NoMod)
 			app.quit();
 
-		auto key_str = key::to_string(k.key, k.modifiers);
-		app.screen().print({ 15, 15 }, fmt::format("Key pressed: {}", key_str), color::Black, color::NoChange);
-
 		if(k.key == key::RIGHT and k.modifiers == key::NoMod)
 		{
 			rotation = std::fmod(std::fmod(rotation + 2.f, 360.f) + 360.f, 360.f);
 			fmt::print(g_log, "rotation: {}\n", rotation);
-			render_demo();
 		}
 		else if(k.key == key::LEFT and k.modifiers == key::NoMod)
 		{
 			rotation = std::fmod(std::fmod(rotation - 2.f, 360.f) + 360.f, 360.f);
 			fmt::print(g_log, "rotation: {}\n", rotation);
-			render_demo();
 		}
 		else if(k.key == key::UP and k.modifiers == key::NoMod)
-		{
 			offset += 0.02f;
-			render_demo();
-		}
 		else if(k.key == key::DOWN and k.modifiers == key::NoMod)
-		{
 			offset -= 0.02f;
-			render_demo();
-		}
+
+		render_demo(k.key, k.modifiers);
 	});
 	app.on_input_event.connect([](const event::Input &c) {
 		fmt::print(g_log, "[main]  input: '{}' 0x{:08x}\n", c.to_string(), std::uint32_t(c.codepoint));
