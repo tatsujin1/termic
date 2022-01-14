0f685fabd9fe27f0c08f54becbe8706a79b13af2
André Jonsson <andre.jonsson@dataductus.se>
2022-01-14 17:47:37 +0100

    using Catch2 for testing

--- tests/text.cpp
+++ tests/text.cpp
@@ -1,40 +1,13 @@
-#include <fmt/core.h>
-
-#include <assert.h>
-
 #include <termic/text.h>
 using namespace  termic;
 
+#include <catch2/catch.hpp>
 
 // TODO: use some testing lib, like gtest
 
-int main(int argc, char *argv[])
-{
-	std::vector<std::string_view> args { argv + 1, argv + argc };
-
-	fmt::print("size: empty\n");
-	{
-		utf8::string s("");
-		assert(text::size(s) == 0);
-	}
-
-	fmt::print("size: ascii\n");
-	{
-		utf8::string s("hello");
-		assert(text::size(s) == 5);
-	}
-
-	fmt::print("size: accent\n");
-	{
-		utf8::string s("héllö");
-		assert(text::size(s) == 5);
-	}
-
-	fmt::print("size: japanese\n");
-	{
-		utf8::string s("利治ミ");
-		assert(text::size(s) == 3);
-	}
-
-	return 0;
+TEST_CASE("Size of UTF-8 strings", "text::size") {
+	REQUIRE( text::size("") == 0 );
+	REQUIRE( text::size("hello") == 5 );
+	REQUIRE( text::size("héllö") == 5 );
+	REQUIRE( text::size("隊ぎやレね") == 5);
 }
