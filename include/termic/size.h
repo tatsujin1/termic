deecf6a8a4ff482a4b978b3115c0db78ffe4587a
André Jonsson <andre.jonsson@dataductus.se>
2022-03-07 10:10:30 +0100

    Added some utility methods for Size, Pos and Rectangle.
    They are entirely stupid, e.g. Pos::move() does not (and can not)
    detect that the resulting position goes off-screen.

--- include/termic/size.h
+++ include/termic/size.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <utility>
 
 namespace termic
 {
@@ -14,12 +15,33 @@ struct Size
 	{
 		return t.width == width and t.height == height;
 	}
+
+	inline Size cap(Size size) const
+	{
+		return {
+			size.width > 0 and size.width < width? size.width: width,
+			size.height > 0 and size.height < height? size.height: height,
+		};
+	}
+
+	std::size_t area() const
+	{
+		return width*height;
+	}
 };
 
 struct Pos
 {
 	std::size_t x;
 	std::size_t y;
+
+	inline Pos move(std::pair<int, int> delta) const
+	{
+		return {
+			static_cast<decltype(x)>(static_cast<int>(x) + delta.first),
+			static_cast<decltype(y)>(static_cast<int>(y) + delta.second),
+		};
+	}
 };
 
 struct Rectangle
@@ -30,6 +52,21 @@ struct Rectangle
 	inline bool contains(Pos pos) const
 	{
 		return pos.x >= top_left.x and pos.x < (top_left.x + size.width) and pos.y >= top_left.y and pos.y < (top_left.y + size.height);
+	}
+
+	inline std::size_t area() const
+	{
+		return size.area();
+	}
+
+	inline Rectangle move(std::pair<int, int> delta) const
+	{
+		return { top_left.move(delta), size };
+	}
+
+	inline Rectangle cap(Size size) const
+	{
+		return { top_left, this->size.cap(size) };
 	}
 };
 
